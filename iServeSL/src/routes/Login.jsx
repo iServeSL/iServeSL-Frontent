baa45cd8784f213ad02af3fe03babc94b9b23039
K.D.S. Akash <akashmusix01@gmail.com>
2024-03-16 04:36:59 +0530

    Route to dashboard

--- iServeSL/src/routes/Login.jsx
+++ iServeSL/src/routes/Login.jsx
@@ -13,6 +13,10 @@ const Login = () => {
         navigate('/');
     }
 
+    const dashboardNavigate = () => {
+        navigate('/dashboard');
+    }
+
     return (
         <section className="bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -45,7 +49,7 @@ const Login = () => {
                                 </div>
                                 <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                             </div>
-                            <button type="submit" className="btnHoverEffect w-full text-white bg-[#ff7300] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                            <button type="submit" onClick={dashboardNavigate} className="btnHoverEffect w-full text-white bg-[#ff7300] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                 Don’t have an account yet? <a onClick={registerNavigate} className="font-medium text-primary-600 hover:underline dark:text-primary-500 cursor-pointer">Sign up</a>
                             </p>
