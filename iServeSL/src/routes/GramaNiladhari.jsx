cc22431ba91d5fc1f78d67f8734b7f5613101d73
K.D.S. Akash <akashmusix01@gmail.com>
2024-03-31 11:57:27 +0530

    Input field labels for request certificate

--- iServeSL/src/routes/GramaNiladhari.jsx
+++ iServeSL/src/routes/GramaNiladhari.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const GramaNiladhari = () => {
   return (
-    <form class="max-w-md mx-auto">
+    <form class="max-w-md mx-auto mt-10">
       <div className="relative z-0 w-full mb-5 group">
         <input
           type="email"
@@ -16,14 +16,14 @@ const GramaNiladhari = () => {
           for="floating_email"
           className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
-          Email address
+          NIC
         </label>
       </div>
       <div className="relative z-0 w-full mb-5 group">
         <input
-          type="password"
-          name="floating_password"
-          id="floating_password"
+          type="text"
+          name="phone"
+          id="phone"
           className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           placeholder=" "
           required
@@ -32,23 +32,23 @@ const GramaNiladhari = () => {
           for="floating_password"
           className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
-          Password
+          Phone
         </label>
       </div>
       <div className="relative z-0 w-full mb-5 group">
         <input
-          type="password"
-          name="repeat_password"
-          id="floating_repeat_password"
+          type="text"
+          name="postal"
+          id="postal"
           className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           placeholder=" "
           required
         />
         <label
-          for="floating_repeat_password"
+          for="floating_password"
           className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
-          Confirm password
+          Postal Code
         </label>
       </div>
       <div className="grid md:grid-cols-2 md:gap-6">
@@ -65,7 +65,7 @@ const GramaNiladhari = () => {
             for="floating_first_name"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            First name
+            No
           </label>
         </div>
         <div className="relative z-0 w-full mb-5 group">
@@ -81,7 +81,7 @@ const GramaNiladhari = () => {
             for="floating_last_name"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Last name
+            Street
           </label>
         </div>
       </div>
@@ -100,7 +100,7 @@ const GramaNiladhari = () => {
             for="floating_phone"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Phone number (123-456-7890)
+            Village
           </label>
         </div>
         <div className="relative z-0 w-full mb-5 group">
@@ -116,7 +116,7 @@ const GramaNiladhari = () => {
             for="floating_company"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Company (Ex. Google)
+            City
           </label>
         </div>
       </div>
@@ -124,7 +124,7 @@ const GramaNiladhari = () => {
         type="submit"
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
-        Submit
+        Request Certificate
       </button>
     </form>
   );
