744fa733e9290b9eafa3470708d603cc491de992
K.D.S. Akash <akashmusix01@gmail.com>
2024-04-29 22:11:18 +0530

    Add edit profile functionality

--- iServeSL/src/components/Profile.jsx
+++ iServeSL/src/components/Profile.jsx
@@ -1,49 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ProfileHeader from "./ProfileHeader";
+import axios from "axios";
+import Cookies from "js-cookie";
 import "../styles/profile.css";
 import { BiEnvelope, BiLock, BiMobile, BiUser, BiPen } from "react-icons/bi";
 
-const details = [
-  {
-    title: "Username",
-    data: "sachinakash_",
-    icon: <BiUser />,
-  },
-  {
-    title: "Email",
-    data: "sachin@wso2.com",
-    icon: <BiEnvelope />,
-  },
-  {
-    title: "Profession",
-    data: "Software Engineer",
-    icon: <BiPen />,
-  },
-  {
-    title: "Password",
-    data: "********",
-    icon: <BiLock />,
-  },
-  {
-    title: "Contact",
-    data: "+94783439022",
-    icon: <BiMobile />,
-  },
-];
-
 const Profile = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [profession, setProfession] = useState("");
+  const [contact, setContact] = useState("");
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userEmail = Cookies.get("email");
+        if (userEmail) {
+          setEmail(userEmail);
+          const response = await axios.get(
+            `http://localhost:3001/api/users/${userEmail}`
+          );
+          const userData = response.data;
+          setUsername(userData.username);
+          setProfession(userData.profession);
+          setContact(userData.contact);
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  const details = [
+    {
+      title: "Username",
+      data: username,
+      icon: <BiUser />,
+    },
+    {
+      title: "Email",
+      data: email,
+      icon: <BiEnvelope />,
+    },
+    {
+      title: "Profession",
+      data: profession,
+      icon: <BiPen />,
+    },
+    {
+      title: "Password",
+      data: "********",
+      icon: <BiLock />,
+    },
+    {
+      title: "Contact",
+      data: contact,
+      icon: <BiMobile />,
+    },
+  ];
+
   return (
     <div className="profile">
       <ProfileHeader />
       <div className="user--profile">
         <div className="user-details">
-          {details.map((details) => (
-            <div className="user">
+          {details.map((detail, index) => (
+            <div key={index} className="user">
               <div className="user-detail">
-                <div className="user-cover">{details.icon}</div>
+                <div className="user-cover">{detail.icon}</div>
                 <div className="user-name">
-                  <h5 className="title">{details.title}</h5>
-                  <span className="duration">{details.data}</span>
+                  <h5 className="title">{detail.title}</h5>
+                  <span className="duration">{detail.data}</span>
                 </div>
               </div>
               <div className="action">:</div>
