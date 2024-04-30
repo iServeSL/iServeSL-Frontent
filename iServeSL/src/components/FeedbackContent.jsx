e69ee7bbbe397988f058de56b58ac4c8d82f30b2
K.D.S. Akash <akashmusix01@gmail.com>
2024-04-30 09:46:11 +0530

    Integration with node server for feedback service

--- iServeSL/src/components/FeedbackContent.jsx
+++ iServeSL/src/components/FeedbackContent.jsx
@@ -1,21 +1,60 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "../styles/content.css";
 
 const FeedbackContent = () => {
   const [subject, setSubject] = useState("");
   const [feedback, setFeedback] = useState("");
+  const [subjectError, setSubjectError] = useState("");
+  const [feedbackError, setFeedbackError] = useState("");
 
   const handleSubjectChange = (e) => {
     setSubject(e.target.value);
+    if (e.target.value.trim() !== "") {
+      setSubjectError("");
+    }
   };
 
   const handleFeedbackChange = (e) => {
     setFeedback(e.target.value);
+    if (e.target.value.trim() !== "") {
+      setFeedbackError("");
+    }
   };
 
-  const handleSubmit = () => {
-    console.log("Subject:", subject);
-    console.log("Feedback:", feedback);
+  const handleSubmit = async () => {
+    let isValid = true;
+
+    if (subject.trim() === "") {
+      setSubjectError("Subject cannot be empty");
+      isValid = false;
+    }
+
+    if (feedback.trim() === "") {
+      setFeedbackError("Feedback cannot be empty");
+      isValid = false;
+    }
+
+    if (isValid) {
+      try {
+        const response = await axios.post(
+          "http://localhost:3001/api/send-email",
+          {
+            subject: subject,
+            feedback: feedback,
+          }
+        );
+        if (response.status === 200) {
+          alert("Thanks for sharing your feedback with us.");
+        } else {
+          alert("Feedback submission unsuccessful! Please try again.");
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+      setSubject("");
+      setFeedback("");
+    }
   };
 
   return (
@@ -37,6 +76,9 @@ const FeedbackContent = () => {
             className="feedback-input bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4"
             placeholder="Enter subject"
           />
+          {subjectError && (
+            <p className="text-red-500 text-xs mt-1">{subjectError}</p>
+          )}
         </div>
         <div className="form-group">
           <label htmlFor="feedback" className="block mb-2 font-bold">
@@ -51,6 +93,9 @@ const FeedbackContent = () => {
             rows="5"
             placeholder="Enter your feedback"
           />
+          {feedbackError && (
+            <p className="text-red-500 text-xs mt-1">{feedbackError}</p>
+          )}
         </div>
         <div align="right">
           <button
