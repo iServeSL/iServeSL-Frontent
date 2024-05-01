665871a25673ba27b04a8850e703b066f5af1c13
K.D.S. Akash <akashmusix01@gmail.com>
2024-05-01 12:40:14 +0530

    Remove unnecessary project files and folders

--- test-project/src/routes/Home.tsx
+++
@@ -1,9 +0,0 @@
-//SachinAkash01 - 2024
-import React from "react";
-import Navigation from "../components/Navigation";
-
-const Home = () => {
-  return <Navigation linkActive="active" />;
-};
-
-export default Home;
