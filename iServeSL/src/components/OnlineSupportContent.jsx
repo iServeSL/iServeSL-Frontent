81a444be912ee17661b89a60a2ca5478a504fc17
Sachin Akash <kdsakash2001@gmail.com>
2024-05-16 14:48:11 +0530

    Add License Header

--- iServeSL/src/components/OnlineSupportContent.jsx
+++ iServeSL/src/components/OnlineSupportContent.jsx
@@ -1,3 +1,4 @@
+// Copyright ©2024 SachinAkash01, All rights reserved.
 import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { ChatFeed, Message } from "react-chat-ui";
